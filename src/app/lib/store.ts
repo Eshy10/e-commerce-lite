5022fb1c852f2518d87e1510ed5d5279f0638058
Eshy10 <adewalebose15@gmail.com>
2024-01-31 23:52:50 +0100

    chore: remove unused code

--- src/app/lib/store.ts
+++ src/app/lib/store.ts
@@ -1,17 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { cartReducer } from './features/cart'
 import { productReducer } from './features/products'
-import { productApi } from './services/product'
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
     cartReducer,
-    [productApi.reducerPath]: productApi.reducer,
     productReducer,
     
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({}).concat(productApi.middleware),
+    }
   })
 }
 
